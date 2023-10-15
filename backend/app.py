9d301ab5eaf032bbff86544757bc8efbad9be094
Sahan Yalavarthi <sahanyalavarthi@gmail.com>
2023-10-14 19:43:58 -0500

    Set up the basis. In app.py, set up the two homepages and what requests they may need, in resources.py, set up what each request does. Also set up the database model for a flashcard. There are two html files in templates that y'all can use for the html pages for the two webpages.

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,34 @@
-from flask import Flask
-# from flask_restful import Api, Resource # API to interact with front-end
-# from flask_cors import CORS # To allow cross-origin requests from frontend to backend
-# from flask_sqlalchemy import SQLAlchemy # To allow for us to use SQLite as our database
+from flask import Flask, render_template, request, redirect, url_for
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route('/') # This will tell the program to default to the home page
-def cardify_home():
-    return 'Welcome to <h1>Cardify\'s<h1> main page. If this is shown, there is no error with the rest api or the sqlite database.'
+# Configuration settings
+app.config.from_object('config.Config')
+
+db = SQLAlchemy(app)
+api = Api(app)
+
+from resources import FlashcardResource
+api.add_resource(FlashcardResource, '/flashcards/<int:flashcard_id>')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        # Handle file upload, extract flashcards, and store in the database
+        # Redirect to the flashcard listing page
+        return redirect(url_for('flashcard_listing'))
+
+    return render_template('home.html', title='Home Page')
+
+# Route for the flashcard listing page
+@app.route('/flashcards', methods=['GET'])
+def flashcard_listing():
+    # Query the database to retrieve flashcards
+    flashcards = Flashcard.query.all()
+    return render_template('flashcard_listing.html', title='Flashcards', flashcards=flashcards)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# # Creating an API object:
-# api = Api(app)
-# # NOTE: WE STILL NEED TO CREATE RESOURCES, ADD THE RESOURCES TO THE API, DEFINE URL ROUTES, ETC.
-
-
-# # Configuring the Database:
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cardify_database.db'  # Replace with your desired database name
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
-# # NOTE: WE STILL NEED TO DEFINE A DATABASE MODEL, CREATE DATABASE TABLES, ETC. --> Closing database connections as well?
--- backend/models.py
+++ backend/models.py
@@ -1,17 +1,13 @@
-# models.py
-
 from app import db
-
 
 class Flashcard(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    front_text = db.Column(db.String(200), nullable=False)
-    back_text = db.Column(db.String(200), nullable=False)
-
-    def __init__(self, front_text, back_text, id):
+    front_text = db.Column(db.String(200), nullable=False) # Question
+    back_text = db.Column(db.String(200), nullable=False) # Answer
+    
+    def __init__(self, front_text, back_text):
         self.front_text = front_text
         self.back_text = back_text
-        self.id = id
-    
+
     def __repr__(self):
         return f'<Flashcard {self.front_text}>'
