8f9ae55d5381b6efbba5ae651c598198010ef119
richiez2 <richiez2@illinois.edu>
2023-10-14 20:16:29 -0500

    organized frontend workspace into components

--- frontend/my-react-app/src/App.js
+++ frontend/my-react-app/src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
+import new_logo from './notecard.png'
 import './App.css';
+import Converter from "./components/converter.js";
 
 function App() {
   return (
     <div className="App">
+      <div className = "Logo">
+      <img src={new_logo} className="App-logo" alt="logo" />
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Converter />
       </header>
     </div>
   );
