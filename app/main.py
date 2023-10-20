e8d53d5098608a7bb4f8b63d557119fe7ba8145b
jonah410 <blackjonah410@gmail.com>
2023-10-20 17:42:27 -0500

    third model's a charm.  See comments at bottom of main.py for next steps

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
-import numpy as np
-import requests
+import g4f
 import PyPDF2
 
-# API endpoint (this is hypothetical and may vary based on actual API details)
-API_ENDPOINT = "http://localhost:3001/v1/completions"
+g4f.logging = True # enable logging
+g4f.check_version = False # Disable automatic version checking
+print(g4f.version) # check version
+print(g4f.Provider.Ails.params)  # supported args
+
+# Automatic selection of provider
 
 def extract_text_from_pdf(pdf_path):
     with open(pdf_path, 'rb') as file:
@@ -19,23 +22,34 @@ pdf_path = "/Users/jonahblack/Downloads/Sample_Cover_Letter.pdf"
 # Extract text from the PDF
 notes = extract_text_from_pdf(pdf_path)
 
-data = {
-    "query": "Parse the following text and output relevant information in flashcard format: 1) [front], 2) [back]: " + notes
-}
+query = "Parse the following text and output relevant information in flashcard format: 1) [front], 2) [back]: " + notes
 
-# print(data)
+# streamed completion
+response = g4f.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": query}],
+    stream=True,
+)
 
-headers = {}
+for message in response:
+    print(message, flush=True, end='')
 
-response = requests.post(API_ENDPOINT, json=data, headers=headers)
+''' # normal response
+response = g4f.ChatCompletion.create(
+    model=g4f.models.gpt_4,
+    messages=[{"role": "user", "content": "Hello"}],
+)  # alternative model setting '''
+
 print(response)
 
-# Check for a valid response
-if response.status_code == 200:
+# next step: return the response as a string, parse the response, set the text into the appropriate parameter for the flashcard datatype
+
+# maybe use check for a valid response as well 
+''' if response.status_code == 200:
     result = response.text
     print(result)
     # result_text = response.json()['choices'][0]['text']
     #  print(result_text)
 
 else:
-    print(f"Error: {response.status_code} - {response.text}")
+    print(f"Error: {response.status_code} - {response.text}") '''
