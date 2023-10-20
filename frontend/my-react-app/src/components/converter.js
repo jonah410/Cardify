0b972aeba233fe8b5c5b11a0cda8a9e8758973c9
richiez2 <richiez2@illinois.edu>
2023-10-20 16:55:05 -0500

    created pdf upload and its integration with backend

--- frontend/my-react-app/src/components/converter.js
+++ frontend/my-react-app/src/components/converter.js
@@ -1,21 +1,14 @@
 import logo from '../logo.svg';
 import new_logo from '../notecard.png'
 import '../App.css';
+import FileUpload from "./fileupload.js";
 const converter = () => {
     return (
         <div className = "App">
             <header className="App-header">
                 <h1>Upload your notes below!</h1>
-                    
+                    <FileUpload/>
                     <button>Convert</button>
-                    <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-                    Learn React
-                    </a>
             </header>
         </div>
     )
