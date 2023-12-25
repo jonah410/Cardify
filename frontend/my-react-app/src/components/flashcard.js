61446b9749fde6d17f4887d58cbd467b250311c5
jonah410 <blackjonah410@gmail.com>
2023-12-25 13:18:11 -0600

    first push of mvp

--- frontend/my-react-app/src/components/flashcard.js
+++ frontend/my-react-app/src/components/flashcard.js
@@ -7,24 +7,32 @@ const styles = {
         color: 'white',
         borderRadius: '20px',
         display: 'flex',
+        flexDirection: 'column', // ensures content is laid out in a single column
         justifyContent: 'center',
         alignItems: 'center',
+        padding: '20px', // adds padding inside the card
+        overflow: 'auto', // adds scrolling to the card if the content is too long
+        fontSize: '16px', // adjust font size as needed
+        textAlign: 'center', // centers text horizontally
+        wordWrap: 'break-word', // ensures that long words don't overflow
+        height: '100%', // makes sure the card fills the container
     },
     allcard: {
         width: '100%',
         height: '100%',
-    }
+    },
 };
+
 
 const Flashcard = ({ front, back }) => {
     return (
-        <div style={{height:'200px', width: '30%'}}>
+        <div style={{height:'200px', width: '30%', margin: '10px'}}>
             <ReactFlipCard
                 containerStyle={styles.allcard}
                 frontStyle={styles.card}
                 backStyle={styles.card}
-                frontComponent={<div>{front}</div>}
-                backComponent={<div>{back}</div>}
+                frontComponent={<div style={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>{front}</div>}
+                backComponent={<div style={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>{back}</div>}
             />
         </div>
     );
